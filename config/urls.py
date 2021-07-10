da72f6e5b95d9d76a9d2c9b59fef6154f3bdba55
dark-codr <jeremiahedavid@gmail.com>
2021-07-10 18:42:04 +0100

    fixed the HOME PAGE N ABOUT US PAGE

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
+    path(
+        "about/team/", TemplateView.as_view(template_name="pages/team.html"), name="team"
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # User URIs
