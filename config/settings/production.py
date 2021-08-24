5bd45e552bb13af021da02665c51a6076a68a8bf
jeremiah david <jeremiahedavids@gmail.com>
2021-08-24 22:11:11 +0100

    added enviroment variable for email host

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,7 +1,7 @@
 import logging
 
 import sentry_sdk
-# from sentry_sdk.integrations.celery import CeleryIntegration
+from sentry_sdk.integrations.celery import CeleryIntegration
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
@@ -253,7 +253,7 @@ sentry_logging = LoggingIntegration(
 integrations = [
     sentry_logging,
     DjangoIntegration(),
-    # CeleryIntegration(),
+    CeleryIntegration(),
     RedisIntegration(),
 ]
 sentry_sdk.init(
