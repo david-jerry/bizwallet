ef9d0f4f15de9cb45568be82edc65a376da294c1
jeremiah david <jeremiahedavids@gmail.com>
2021-08-24 03:46:22 +0100

    saved home

--- config/settings/production.py
+++ config/settings/production.py
@@ -56,13 +56,13 @@ SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 SECURE_SSL_HOST = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
 SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_SAMESITE = "Strict"
+# SESSION_COOKIE_SAMESITE = "Strict"
 # SESSION_COOKIE_DOMAIN = ["www.bizwallet.org", "bizwallet.apps.techydra.ltd"]
 CORS_REPLACE_HTTPS_REFERER = True
 HOST_SCHEME = "https://"
 CSRF_COOKIE_SAMESITE = "Strict"
 # CSRF_TRUSTED_ORIGINS = [".bizwallet.org", "bizwallet.apps.techydra.ltd"]
-SECURE_REFERRER_POLICY = "same-origin"
+# SECURE_REFERRER_POLICY = "same-origin"
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
 CSRF_COOKIE_SECURE = True
